7798a3ac0a06a8819cecad4f21441c1b5e81a708
Limichui <limbert.olmos.m@gmail.com>
2025-10-19 23:43:02 -0400

    Inicio de sesion y crud - Actividad 4

---
+++ actividad4/app/Models/Categoria.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Categoria
+ *
+ * @property $id
+ * @property $nombre
+ * @property $descripcion
+ * @property $estado
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Categoria extends Model
+{
+    
+    protected $perPage = 20;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['nombre', 'descripcion', 'estado'];
+
+
+}
--- actividad4/routes/web.php
+++ actividad4/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('categorias', App\Http\Controllers\CategoriaController::class);
